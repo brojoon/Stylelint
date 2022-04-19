1a3cc06a44015e7d50427faf38ab21fea3b4a236
brojoon <ydngjink1234@gmail.com>
2022-04-20 04:56:56 +0900

    refactor: jwtConstants 일부 수정

--- back/src/auth/constants.ts
+++ back/src/auth/constants.ts
@@ -2,8 +2,5 @@ export const jwtConstants = {
   secret: process.env.SECRET,
   expirationTime: process.env.EXPIRATIONTIME,
 
-  KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
-  KAKAO_SECRET: '',
-  KAKAO_CALLBACK_URL: process.env.KAKAO_CALLBACK_URL,
   HOME: 'http://localhost:3000',
 };
