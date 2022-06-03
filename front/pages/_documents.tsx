85868101ddc96df93d27bbba78dbaec7dcfb07ab
brojoon <ydngjink1234@gmail.com>
2022-06-04 07:39:04 +0900

    refactor: meta 태그 추가

--- front/pages/_documents.tsx
+++ front/pages/_documents.tsx
@@ -37,8 +37,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="author" content="Stylelint">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta charset="UTF-8" />
+          <title>Stylelint</title>
+          {/* <meta http-equiv="X-UA-Compatible" content="IE=edge" /> */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="author" content="Stylelint" />
+          <meta name="apple-mobile-web-app-title" content="Stylelint" />
+          <meta name="description" content="여성 의류 쇼핑몰 Stylelint 입니다." />
+          <meta property="og:title" content="스타일린트" />
+          <meta property="og:url" content="https://www.stylelint.ml/" />
+          <meta property="og:description" content="여성 의류 쇼핑몰 Stylelint 입니다." />
+          {/* <meta property="og:img" content=""> */}
+          <meta property="twitter:card" content="summary" />
         </Head>
         <body>
           <Main />
